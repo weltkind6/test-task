6bdefd4553313a86b31e3a4da168077d821dd125
weltkind6 <yaroslavzef@icloud.com>
2021-06-09 12:27:31 +0300

    initial

---
+++ test/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react'
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Test app</h1>
+        </div>
+    );
+}
+
+export default App;
