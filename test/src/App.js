b48ac4ddee5142e95a85018dd171fc0bf5d3c24f
weltkind6 <yaroslavzef@icloud.com>
2021-06-11 12:32:57 +0300

    toggle refactor

--- test/src/App.js
+++ test/src/App.js
@@ -9,6 +9,12 @@ function App() {
     const [state, setState] = useState([])
     const [toggle, setToggle] = useState(true)
     const [receiving, setReceiving] = useState(true)
+    const [title, setColumnTitle] = React.useState('')
+
+    const sortData = (tableTitle) => {
+        sortDataHandler(tableTitle)
+        setColumnTitle(tableTitle)
+    }
 
     useEffect(() => {
         axios('http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D')
@@ -17,6 +23,13 @@ function App() {
                 setReceiving(false)
             })
     }, [])
+
+    const sortDataHandler = (callBack) => {
+        setToggle(!toggle)
+        if (toggle) {
+            setState([...state.sort((a, b) => a[callBack] > b[callBack] ? 1 : -1)])
+        } else setState([...state.sort((a, b) => a[callBack] < b[callBack] ? 1 : -1)])
+    }
 
 
     return (
@@ -27,6 +40,9 @@ function App() {
                 setState={setState}
                 toggle={toggle}
                 setToggle={setToggle}
+                sortDataHandler={sortDataHandler}
+                title={title}
+                sortData={sortData}
             />
         </div>
     );
--- test/src/Components/Table/Table.jsx
+++ test/src/Components/Table/Table.jsx
@@ -1,61 +1,50 @@
 import React from 'react';
 import './Table.css'
-import ArrowUp from "../ToggleArrow/ArrowUp/ArrowUp";
-import ArrowEnd from "../ToggleArrow/ArrowEnd/ArrowEnd";
 import ToggleArrow from "../ToggleArrow/ToggleArrow";
 
-const Table = ({state, setState, toggle, setToggle}) => {
-
-    const sortDataHandler = (callBack) => {
-        setToggle(!toggle)
-        if (toggle) {
-            setState([...state.sort((a, b) => a[callBack] > b[callBack] ? 1 : -1)])
-        } else setState([...state.sort((a, b) => a[callBack] < b[callBack] ? 1 : -1)])
-    }
+const Table = ({state, toggle, title, sortData}) => {
 
 
     return (
+
         <table className="table">
             <thead className="thead-dark">
             <tr>
-                <th scope="col" onClick={() => sortDataHandler('id')}>
+                <th scope="col" onClick={() => sortData('id')}>
                     ID
-                    <ToggleArrow toggle={toggle}/>
+                    {title === 'id' ? <ToggleArrow toggle={toggle}/> : null}
                 </th>
-                <th scope="col" onClick={() => sortDataHandler('firstName')}>
+                <th scope="col" onClick={() => sortData('firstName')}>
                     FirstName
-                    <ToggleArrow toggle={toggle}/>
+                    {title === 'firstName' ? <ToggleArrow toggle={toggle}/> : null}
                 </th>
-                <th scope="col" onClick={() => sortDataHandler('lastName')}>
+                <th scope="col" onClick={() => sortData('lastName')}>
                     LastName
-                    <ToggleArrow toggle={toggle}/>
+                    {title === 'lastName' ? <ToggleArrow toggle={toggle}/> : null}
                 </th>
-                <th scope="col" onClick={() => sortDataHandler('email')}>
+                <th scope="col" onClick={() => sortData('email')}>
                     Email
-                    <ToggleArrow toggle={toggle}/>
+                    {title === 'email' ? <ToggleArrow toggle={toggle}/> : null}
                 </th>
-                <th scope="col" onClick={() => sortDataHandler('phone')}>
+                <th scope="col" onClick={() => sortData('phone')}>
                     Phone
-                    <ToggleArrow toggle={toggle}/>
+                    {title === 'phone' ? <ToggleArrow toggle={toggle}/> : null}
                 </th>
             </tr>
             </thead>
 
             <tbody>
-            {state.map(i =>
-                <tr key={i.id}>
-                    <td>{i.id + Math.floor(Math.random() * 100)}</td>
+            {state.map((i, index) =>
+                <tr key={index}>
+                    <td>{i.id}</td>
                     <td>{i.firstName}</td>
                     <td>{i.lastName}</td>
                     <td>{i.email}</td>
                     <td>{i.phone}</td>
                 </tr>)}
-
             </tbody>
         </table>
     );
 };
 
 export default Table;
-// {toggle ? <ArrowUp /> : <ArrowEnd />}
-// setState([...state.sort((a, b) => a[callBack] < b[callBack] ? 1 : -1)])
